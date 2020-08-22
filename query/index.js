96ad9cb5193966ef3d9193e6cc774708eee2e964
allu <aleksijaakkojuhani@gmail.com>
2020-08-22 14:40:22 +0300

    adding query service

---
+++ query/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { response } = require("express");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] }
+    
+  } else if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+    posts[postId].comments.push({ id, content });
+  }
+
+  console.log("POSTS DATA=>", posts);
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log("listening on 4002!!");
+});
